1739ee947c8d8ab7fec15c2f285cbd49a6b15580
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-22 19:39:08 -0800

    solved 0122 challenge

---
+++ DailyChallenge/Jan/260122.js
@@ -0,0 +1,48 @@
+// Class Average
+// Given an array of exam scores (numbers), return the average score in form of a letter grade according to the following chart:
+
+// Average Score	Letter Grade
+// 97-100	"A+"
+// 93-96	"A"
+// 90-92	"A−"
+// 87-89	"B+"
+// 83-86	"B"
+// 80-82	"B-"
+// 77-79	"C+"
+// 73–76	"C"
+// 70-72	"C-"
+// 67-69	"D+"
+// 63-66	"D"
+// 60–62	"D-"
+// below 60	"F"
+// Calculate the average by adding all scores in the array and dividing by the total number of scores.
+
+function getAverageGrade(scores) {
+    let total = 0;
+    for(let i = 0; i < scores.length; i++){
+        total += scores[i];
+    }
+
+    const average = total / scores.length;
+
+    if(average >= 97)      return "A+";
+    else if(average >= 93) return "A";
+    else if(average >= 90) return "A-";
+    else if(average >= 87) return "B+";
+    else if(average >= 83) return "B";
+    else if(average >= 80) return "B-";
+    else if(average >= 77) return "C+";
+    else if(average >= 73) return "C";
+    else if(average >= 70) return "C-";
+    else if(average >= 67) return "D+";
+    else if(average >= 63) return "D";
+    else if(average >= 60) return "D-";
+    else return "F";
+}
+
+console.log(getAverageGrade([92, 91, 90, 94, 89, 93]));
+console.log(getAverageGrade([84, 89, 85, 100, 91, 88, 79]));
+console.log(getAverageGrade([63, 69, 65, 66, 71, 64, 65]));
+console.log(getAverageGrade([97, 98, 99, 100, 96, 97, 98, 99, 100]));
+console.log(getAverageGrade([75, 100, 88, 79, 80, 78, 64, 60]));
+console.log(getAverageGrade([45, 48, 50, 52, 100, 54, 56, 58, 59]));
