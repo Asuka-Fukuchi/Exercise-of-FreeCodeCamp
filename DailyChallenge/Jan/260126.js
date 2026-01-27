668c95a071bcf2b9b779a38f2603380735e9c81d
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-26 19:41:33 -0800

    solved 0126 challenge

---
+++ DailyChallenge/Jan/260126.js
@@ -0,0 +1,25 @@
+// FizzBuzz Mini
+// Given an integer, return a string based on the following rules:
+
+// If the number is divisible by 3, return "Fizz".
+// If the number is divisible by 5, return "Buzz".
+// If the number is divisible by both 3 and 5, return "FizzBuzz".
+// Otherwise, return the given number as a string.
+
+function fizzBuzzMini(n) {
+    if(n % 3 === 0 && n % 5 ===0){
+        return "FizzBuzz";
+    } else if(n % 3 === 0){
+        return "Fizz";
+    } else if(n % 5 === 0){
+        return "Buzz";
+    } else {
+        return n.toString();
+    }
+}
+
+console.log(fizzBuzzMini(3));
+console.log(fizzBuzzMini(4));
+console.log(fizzBuzzMini(35));
+console.log(fizzBuzzMini(75));
+console.log(fizzBuzzMini(98));
