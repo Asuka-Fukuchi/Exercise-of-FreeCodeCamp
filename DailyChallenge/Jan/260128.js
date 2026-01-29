a38d739ffeed967cc0d3b61e2b424af73e5a7e2c
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-28 19:12:31 -0800

    solved 0128 challenge

---
+++ DailyChallenge/Jan/260128.js
@@ -0,0 +1,13 @@
+// Flatten the Array
+// Given an array that contains nested arrays, return a new array with all values flattened into a single, one-dimensional array. Retain the original order of the items in the arrays.
+
+function flatten(arr) {
+  const flattened = arr.flat(Infinity);
+  return flattened;
+}
+
+console.log(flatten([1, [2, 3], 4]));
+console.log(flatten([5, [4, [3, 2]], 1]));
+console.log(flatten(["A", [[[["B"]]]], "C"]));
+console.log(flatten([["L", "M", "N"], ["O", ["P", "Q", ["R", ["S", ["T", "U"]]]]], "V", ["W", ["X", ["Y", ["Z"]]]]]));
+console.log(flatten([["red", ["blue", ["green", ["yellow", ["purple"]]]]], "orange", ["pink", ["brown"]]]));
